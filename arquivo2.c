15ef47e9567064b6d5267944d2420fe579616bb1
ZeusMoreira <zeus.moreira111@gmail.com>
2021-10-25 02:58:13 -0400

    new file

---
+++ arquivo2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("1+2=3");
+    printf("2+3=5");
+    printf("3+5=8");
+    printf("5+8=13");
+    printf("8+13=21");
+    printf("Ola, mundo!");
+    return 0;
+}
