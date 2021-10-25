6d5f316e55604c9eb6c3b3afc9f26db02da2de05
ZeusMoreira <zeus.moreira111@gmail.com>
2021-10-25 03:07:58 -0400

    aaaaaaaa

--- arquivo2.c
+++ arquivo2.c
@@ -5,7 +5,7 @@ int main(){
     printf("2+3=5");
     printf("3+5=8");
     printf("5+8=13");
-    printf("8+13=21");
+    printf("8+13=21aaaaaaaaaaaaaaaaaaa");
     printf("Ola, mundo!");
     return 0;
 }
